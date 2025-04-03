0bb87f3918a321cd3fc9b39ec33522ce3e49972e
work <shadim115115@gmail.com>
2025-04-03 16:46:20 +0300

    added render options

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,20 @@ const postgresPort = env('POSTGRESDB_PORT')
 const postgresUser = env('POSTGRESDB_USER')
 const postgresPassword = env('POSTGRESDB_PASSWORD')
 const postgresName = env('POSTGRESDB_NAME')
+const postgresUrl = env('DATABASE_URL')
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
+      url: postgresUrl,
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false, // відображення SSL-помилок
+        },
+      },
       host: postgresHost || 'localhost',
       port: parseInt(postgresPort, 10) || 5432,
       username: postgresUser,
